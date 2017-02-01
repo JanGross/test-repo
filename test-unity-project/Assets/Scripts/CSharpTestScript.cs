b524063005490606e5ca40a4dd9cb864393a387d
Jan Groß <minzkraut@gmail.com>
2017-02-01 19:30:21 +0100

    Basic setup and gitignore

---
+++ test-unity-project/Assets/Scripts/CSharpTestScript.cs
@@ -0,0 +1,23 @@
+﻿/* Author: Jan Groß
+ * 
+ * C# Script for tests
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSharpTestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.gameObject.transform.Rotate (new Vector3 (0, 100 * Time.deltaTime, 0));
+	}
+}
